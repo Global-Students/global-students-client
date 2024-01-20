70e3c58ba43f55f578e855ff60698b021feeacf5
haesa <xdhaesa@gmail.com>
2024-01-21 02:24:44 +0900

    #4 [FEAT] 회원가입 완료 뷰 구현 완료

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,8 @@ module.exports = {
         DEFAULT: '0px 5px 30px -15px rgba(0, 0, 0, 0.20)',
         'inner-orange':
           '-12px -12px 14.3px 1px rgba(255, 85, 0, 0.20) inset, -2px -2px 6.7px 0px rgba(255, 255, 255, 0.25) inset, 3px 3px 5px -2px rgba(255, 255, 255, 0.25) inset',
+        welcome:
+          '-12px -12px 14.3px 1px rgba(255, 85, 0, 0.20) inset, -2px -2px 6.7px 0px rgba(255, 255, 255, 0.25) inset, 3px 3px 5px -2px rgba(255, 255, 255, 0.25) inset, 0px 4px 15px 0px rgba(0, 0, 0, 0.10)',
         'prev-btn':
           '4px 4px 10px -3px rgba(0, 0, 0, 0.06), -4px -4px 9.8px -9px rgba(0, 0, 0, 0.03) inset, -1px -1px 30px 0px #FFF inset',
       },
