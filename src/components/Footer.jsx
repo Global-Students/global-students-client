12d340973f3bfacb684f029b0f490bce0d03d074
munjji <ehowl1117@naver.com>
2024-01-24 19:44:21 +0900

    #8 [STYLE] Footer CSS 정렬 재수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,8 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className='flex justify-center'>
-      <div className='flex flex-col w-[1279px] h-[17px] absolute text-gray-scale-5'>
-        <div className='w-[1279px] h-[0px] top-0 border border-gray-scale-5' />
-        <div className='flex flex-row gap-x-[33.06px]'>
+      <div className='w-[1279px] border-t border-gray-scale-5 text-gray-scale-5'>
+        <div className='w-[255px] flex justify-between'>
           <div className='text-footerFont'>개인정보 처리방침</div>
           <div className='text-footerFont'>이용약관</div>
           <div className='text-footerFont'>운영정책</div>
