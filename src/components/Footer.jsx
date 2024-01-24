0fdf0345c371a866f567d8827534aed0b0a67d19
munjji <ehowl1117@naver.com>
2024-01-24 15:49:30 +0900

    #8 [STYLE] Footer CSS 수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,19 +2,15 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div className='flex justify-center w-[1279px] h-[17px] absolute left-[320px] text-gray-scale-5'>
-      <div className='w-[1279px] h-[0px] left-0 top-0 absolute border border-gray-scale-5' />
-      <div className='left-[1.07px] top-[6px] absolute text-footerFont'>
-        개인정보 처리방침
-      </div>
-      <div className='left-[99.12px] top-[6px] absolute text-footerFont'>
-        이용약관
-      </div>
-      <div className='left-[162.01px] top-[6px] absolute text-footerFont'>
-        운영정책
-      </div>
-      <div className='left-[224.89px] top-[6px] absolute text-footerFont'>
-        공지사항
+    <div className='flex justify-center'>
+      <div className='flex flex-col w-[1279px] h-[17px] absolute text-gray-scale-5'>
+        <div className='w-[1279px] h-[0px] top-0 border border-gray-scale-5' />
+        <div className='flex flex-row gap-x-[33.06px]'>
+          <div className='text-footerFont'>개인정보 처리방침</div>
+          <div className='text-footerFont'>이용약관</div>
+          <div className='text-footerFont'>운영정책</div>
+          <div className='text-footerFont'>공지사항</div>
+        </div>
       </div>
     </div>
   );
