5f2817ed2acaec1128ac8180ba1324d1bb2214d8
mchaeeun <chaeeun@dgu.ac.kr>
2024-01-30 01:54:32 +0900

    #11 [FIX] 마이페이지 위치 조정

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -5,7 +5,7 @@ import Infobox from '../components/Infobox';
 
 export default function MyPage() {
     return (
-  <div className="flex justify-start items-start mx-80 my-[100px] gap-6">
+  <div className="flex justify-start items-start my-[100px] gap-6">
     <ProfileBox nickname='test' bio='test' profileImage='test' />
     <div className="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 relative gap-[45px]">
       <Infobox univ="ㅇㅇ대학교" major="기계공학과" pnumber="010-0000-0000"/>
