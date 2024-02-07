c8b800f592f906509ce32e22627a378a97382fd9
haesa <xdhaesa@gmail.com>
2024-02-08 01:41:52 +0900

    #23 [STYLE] OptionInput 컴포넌트의 placehoder 색상을 text-gray-scale-4로 수정

--- src/components/Input/OptionInput.jsx
+++ src/components/Input/OptionInput.jsx
@@ -11,14 +11,18 @@ export default function OptionInput({
   return (
     <div className='flex border border-gray-scale-7-main rounded px-[19px] py-5 shadow-[0px_5px_30px_-15px_rgba(0, 0, 0, 0.12)]'>
       <select
-        className='flex-1 appearance-none outline-none'
+        className={`flex-1 ${children ? 'appearance-none' : ''} ${
+          value ? '' : 'text-gray-scale-4 font-light'
+        } outline-none`}
         id={id}
         value={value}
         onChange={onChange}
       >
         <option value=''>{placeholder}</option>
         {options.map((option) => (
-          <option value={option}>{option}</option>
+          <option className='text-black' value={option}>
+            {option}
+          </option>
         ))}
       </select>
       <p className='text-gray-scale-4 font-light'>{children}</p>
