1cb91c75fd3cfc2c242c21bb9d944eb28b692084
mchaeeun <chaeeun@dgu.ac.kr>
2024-02-08 22:18:36 +0900

    [ADD] add orange-4 color configuration to tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors: {
         'orange-main': '#FF743D',
         'orange-1': '#FF5500',
+        'orange-4': '#FFA77B40',
         'gray-scale-1': '#1E1E1E',
         'gray-scale-2': '#414244',
         'gray-scale-3': '#50545E',
@@ -27,17 +28,7 @@ module.exports = {
         'check-circle': "url('/src/assets/check_circle.svg')",
         'check-true-circle': "url('/src/assets/check_true_circle.svg')",
       },
-      boxShadow: {
-        DEFAULT: '0px 5px 30px -15px rgba(0, 0, 0, 0.20)',
-        'inner-orange':
-          '-12px -12px 14.3px 1px rgba(255, 85, 0, 0.20) inset, -2px -2px 6.7px 0px rgba(255, 255, 255, 0.25) inset, 3px 3px 5px -2px rgba(255, 255, 255, 0.25) inset',
-        welcome:
-          '-12px -12px 14.3px 1px rgba(255, 85, 0, 0.20) inset, -2px -2px 6.7px 0px rgba(255, 255, 255, 0.25) inset, 3px 3px 5px -2px rgba(255, 255, 255, 0.25) inset, 0px 4px 15px 0px rgba(0, 0, 0, 0.10)',
-        'prev-btn':
-          '4px 4px 10px -3px rgba(0, 0, 0, 0.06), -4px -4px 9.8px -9px rgba(0, 0, 0, 0.03) inset, -1px -1px 30px 0px #FFF inset',
-        dropShadow: '8px 9px 30px rgba(0, 0, 0, 0.08)',
-        message: '1px 2px 8.300000190734863px -3px rgba(0,0,0,0.15)',
-      },
+
       fontSize: {
         headerFont: [
           '15px',
