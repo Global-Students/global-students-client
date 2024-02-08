9821eadc3e1151f3512e538ffb2c71de98ba051e
haesa <xdhaesa@gmail.com>
2024-02-08 14:58:01 +0900

    #31 [REFACTOR] 하드코딩 및 더미 데이터 상수처리

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,9 @@
 import API_PATH from './api';
 import { DATES, MONTHS, YEARS } from './date';
+import { MEMBER_LIST, UNIVERSITY_APPROVAL_LIST } from './dummy';
 import MENU_ID from './id';
 import REGEX from './regex';
+import SCHEMA from './schema';
 import { LABEL, LEGEND, PLACEHOLDER } from './signUpForm';
 
 export {
@@ -9,9 +11,12 @@ export {
   DATES,
   LABEL,
   LEGEND,
+  MEMBER_LIST,
   MENU_ID,
   MONTHS,
   PLACEHOLDER,
   REGEX,
+  SCHEMA,
+  UNIVERSITY_APPROVAL_LIST,
   YEARS,
 };
---
+++ src/constants/schema.js
@@ -0,0 +1,26 @@
+const SCHEMA = Object.freeze({
+  memberList: [
+    '회원 고유 번호',
+    '회원 id',
+    '이름',
+    '닉네임',
+    '생일',
+    '국적',
+    '유학국',
+    '학교',
+    '가입일자',
+    '인증여부',
+    '신고횟수',
+    '상태',
+  ],
+  universityApprovalList: [
+    '회원 고유 번호',
+    '회원 id',
+    '가입일자',
+    '대학교',
+    '인증파일',
+    '인증확인',
+  ],
+});
+
+export default SCHEMA;
