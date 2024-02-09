24c7192721d62cf077474ac5622977450ed2db8b
haesa <xdhaesa@gmail.com>
2024-02-10 01:05:40 +0900

    #23 [STYLE] LightOrangeButton 컴포넌트가 disabled일 때의 스타일 추가

--- src/components/Button/LightOrangeButton.jsx
+++ src/components/Button/LightOrangeButton.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
-export default function LightOrangeButton({ text, onClick }) {
+export default function LightOrangeButton({ text, onClick, disabled = false }) {
+  const buttonStyle =
+    'min-w-[88px] rounded-[9px] px-[16px] py-[12px] mr-[10px] bg-orange-2 bg-opacity-25 text-orange-main hover:brightness-110';
+  const disableStyle =
+    'min-w-[88px] rounded-[9px] px-[16px] py-[12px] mr-[10px] bg-gray-scale-8 text-gray-scale-5';
+
   return (
     <button
-      className='min-w-[88px] bg-[#FFA77B40] rounded-[9px] text-orange-main px-[16px] py-[12px] mr-[10px] hover:brightness-90'
+      className={`${disabled ? disableStyle : buttonStyle}`}
       type='button'
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
