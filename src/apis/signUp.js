a0a94a416d3063aded286dfcf0c5b232e9f99143
haesa <xdhaesa@gmail.com>
2024-02-14 15:29:04 +0900

    #23 [CORRECT] 회원가입 API 요청 header 수정

--- src/apis/signUp.js
+++ src/apis/signUp.js
@@ -36,9 +36,7 @@ const verifyAuthCode = (body) =>
 const submitSignUpInfo = (signUpInfo, moveStep) => {
   axios
     .post(API_PATH.sumbitSignUpInfo, signUpInfo, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      'Content-Type': 'multipart/form-data',
     })
     .then(() => moveStep('welcome'))
     .catch(() => alert('회원가입을 진행할 수 없습니다. 다시 시도해주세요.'));
