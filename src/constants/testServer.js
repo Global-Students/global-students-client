099ee3c18d668b6dcb67d45bd856842ffd314ee7
mchaeeun <chaeeun@dgu.ac.kr>
2024-02-15 01:46:16 +0900

    #39 [FEAT] 마이페이지 조회 API 연결

---
+++ src/constants/testServer.js
@@ -0,0 +1,2 @@
+const URL = 'https://cb05ea7d-8398-40ae-813f-b22ccc381cd1.mock.pstmn.io';
+export default URL;
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import ProfileBox from '../components/ProfileBox';
 import PostTable from '../components/PostTable';
 import Infobox from '../components/Infobox';
+import URL from '../constants/testServer';
 
 export default function MyPage() {
+  const [data, setData] = useState([]);
+  
+  useEffect(() => { 
+    async function fetchData() {
+      try {
+        const response = await axios.get(`${URL}/mypage?userId=00`);
+        setData(response.data.result);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
     return (
   <div className="flex justify-start items-start my-[100px] gap-6">
-    <ProfileBox nickname='test' bio='test' profileImage='test' />
+    <ProfileBox nickname={data.nickname} bio={data.introduction} profileImage={data.profile_photo} />
     <div className="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 relative gap-[45px]">
-      <Infobox univ="ㅇㅇ대학교" major="기계공학과" pnumber="010-0000-0000"/>
+      <Infobox univ={data.host_university} major={data.major} pnumber="010-0000-0000"/>
       <PostTable tablename="내가 쓴 글" link="/dashboard/myPosts"/>
       <PostTable tablename="즐겨찾기한 글" link="/dashboard/bookmarkPosts" />
     </div>
