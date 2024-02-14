3fd7ecfa05c6779a5d2605e4652407be5b61b3cb
haesa <xdhaesa@gmail.com>
2024-02-15 05:50:21 +0900

    #45 [FEAT] 로그아웃 후 홈화면으로 이동

--- src/hooks/useLogout.jsx
+++ src/hooks/useLogout.jsx
@@ -1,12 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import authAxios from '../axios/authAxios';
 import API_PATH from '../constants/api';
 
 export default function useLogout() {
+  const navigate = useNavigate();
   const logout = () =>
-    authAxios.post(API_PATH.logout).then(() => {
-      localStorage.removeItem('accessToken');
-      localStorage.removeItem('expireAt');
-    });
+    authAxios
+      .post(API_PATH.logout) //
+      .then(() => {
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('expireAt');
+        navigate('/');
+      });
 
   return { logout };
 }
