f958371687d20fa117b45273da9c50bcaa6cc709
haesa <xdhaesa@gmail.com>
2024-02-15 20:52:22 +0900

    #70 [REFACTOR] ValidationMessage에서 SVG 파일을 리액트 컴포넌트로 임포트

--- src/components/ValidationMessage.jsx
+++ src/components/ValidationMessage.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ReactComponent as ErrorIcon } from '../assets/error.svg';
+import { ReactComponent as SuccessIcon } from '../assets/success.svg';
 
 export default function ValidationMessage({ message, isValid }) {
   const textStyle = 'text-[14px] font-normal leading';
@@ -9,15 +11,7 @@ export default function ValidationMessage({ message, isValid }) {
         !!message === false ? 'hidden' : 'block'
       } flex items-center gap-[8px] mt-[6px]`}
     >
-      {isValid ? (
-        <img
-          className='w-[20px] h-[20px]'
-          src='/assets/success.svg'
-          alt='success'
-        />
-      ) : (
-        <img src='/assets/error.svg' alt='error' />
-      )}
+      {isValid ? <SuccessIcon /> : <ErrorIcon />}
       <span
         className={`${textStyle} ${
           isValid ? 'text-[#0FC02C]' : 'text-orange-1'
