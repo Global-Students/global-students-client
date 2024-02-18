308b7217087b475fffb6e20cee880813bc1e4c87
munjji <ehowl1117@naver.com>
2024-02-19 06:58:19 +0900

    공지글 스타일 수정

--- src/components/Inform.jsx
+++ src/components/Inform.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Inform({ notice }) {
   return (
     <div className='flex w-[953px] h-[72px] items-center rounded-[14px] border border-gray-scale-7-main mb-[20px]'>
-      <div className='flex flex-row w-[162px] justify-between ml-[29px]'>
+      <div className='flex flex-row justify-between ml-[29px]'>
         <p className='text-orange-main text-lg font-normal'>공지</p>
         <Link
           to={`/post-view/${localStorage.getItem('currentBoardId')}/${
