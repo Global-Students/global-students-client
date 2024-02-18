a41a4478506d3531da73ee3ae076e291079b649b
hxxnjoon <105426026+hxxnjoon@users.noreply.github.com>
2024-02-19 01:59:02 +0900

    #131 [STYLE] Test

--- src/pages/Post/PostView.jsx
+++ src/pages/Post/PostView.jsx
@@ -151,7 +151,6 @@ export default function PostView() {
                     deleteComment={deleteComment}
                   />
                 ))}
-
                 <CommentPagination
                   pageInfo={{
                     page: currentPage,
