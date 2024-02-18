ee880eaa01fcd6e774b1c3da44928df1d44baf1d
hxxnjoon <105426026+hxxnjoon@users.noreply.github.com>
2024-02-19 02:06:59 +0900

    #131 [CORRECT] postId props 수정

--- src/pages/Post/PostView.jsx
+++ src/pages/Post/PostView.jsx
@@ -143,7 +143,7 @@ export default function PostView() {
                     <button type='button'>최신순</button>
                   </div>
                 </div>
-                <WriteComment addComment={addComment} />
+                <WriteComment addComment={addComment} postId={postId}/>
                 {comments.map((comment) => (
                   <Comment
                     key={comment.commentId}
