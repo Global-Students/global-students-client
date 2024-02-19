b37fa414e33401fd58472fc74a567afe1e97fef9
munjji <ehowl1117@naver.com>
2024-02-19 11:01:04 +0900

    검색 결과 텍스트 수정

--- src/components/SearchText.jsx
+++ src/components/SearchText.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import TranslateButton from './Button/TranslateButton';
 
 export default function SearchText() {
+  const params = useParams();
   return (
     <div className='flex flex-col w-[954px]'>
       <div className='flex flex-row justify-between items-center'>
         <div className='flex flex-row justify-between text-[26px] font-bold'>
           <img src='/assets/manage_search.svg' alt='검색 결과' />
           <p className='text-orange-1'>
-            &nbsp;&quot;<u>{localStorage.getItem('q')}</u>&quot;
+            &nbsp;&quot;<u>{params.keyword}</u>&quot;
           </p>
           <p className=''>&nbsp;검색 결과입니다.</p>
         </div>
